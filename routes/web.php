a529d5a0aabfa556d53342d878119e4167cfe657
CedXD <cedricchandra99@gmail.com>
2025-10-19 23:31:13 +0700

    qris&bank

--- routes/web.php
+++ routes/web.php
@@ -2,3 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('pages.home');
+});
+
+Route::get('/home', function () {
+    return view('pages.home');
+});
