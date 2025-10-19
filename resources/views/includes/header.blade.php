b98f25c9f7c04c790cfae09dc99134bcb10faa96
Aloysius Abian Dwi Hananta <aloysiusbian@gmail.com>
2025-10-19 21:11:18 +0700

    fix logo

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,12 +1,12 @@
 <header>
     <nav class="navbar navbar-expand-lg bg-warning shadow-sm">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">
-                <img src="{{ asset('images/image.png') }}" alt="Agung Logo" height="40">
+            <a class="navbar-brand" href="/home">
+                <img src="{{ asset('images/logo.png') }}" alt="Agung Logo" height="40">
             </a>
 
             <div class="input-group flex-grow-1 mx-lg-5">
-                <input class="form-control rounded-pill border-0" placeholder="Bakpia Pathok?" aria-label="Search"">
+                <input class="form-control rounded-pill border-0" placeholder="Bakpia Pathok?" aria-label="Search">
                 <span class="input-group-text bg-transparent border-0 position-absolute end-0" style="z-index: 1;" >
                     <i class="bi bi-search text-secondary"></i>
                 </span>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('auth.login');
 });
+
+Route::get('/home', function () {
+    return view('pages.home');
+});
