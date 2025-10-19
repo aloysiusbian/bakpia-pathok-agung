7a720dbf3e628de8a53f5c18ad86e3cf2007fef7
CedXD <cedricchandra99@gmail.com>
2025-10-19 23:35:53 +0700

    perbaikanQris

--- resources/views/pages/qris.blade.php
+++ resources/views/pages/qris.blade.php
@@ -1,38 +1,5 @@
 @extends('templates.app')
 
-<<<<<<< HEAD
-@section('title', 'Qris')
-
-@section('content')
-<div class="payment-box">
-  <!-- QR CODE -->
-  <img src="{{ asset('images/qris_sample.png') }}" alt="QRIS QR Code" width="180" height="180">
-
-  <!-- QRIS Logo -->
-  <div>
-    <img src="{{ asset('images/qris.png') }}" alt="Pembayaran 1" class="h-8" height="100" background-color=transparant>
-  </div>
-
-  <!-- Detail pembayaran -->
-  <table>
-    <tr>
-      <td><strong>Total Pesanan (1 Menu)</strong></td>
-      <td class="text-end">Rp 250.000</td>
-    </tr>
-    <tr>
-      <td><strong>Total Biaya Pengiriman</strong></td>
-      <td class="text-end">Rp 10.000</td>
-    </tr>
-    <tr>
-      <td><strong>Total</strong></td>
-      <td class="text-end fw-bold">Rp 260.000</td>
-    </tr>
-  </table>
-
-  <!-- Tombol Status -->
-  <button class="btn-status">Menunggu Konfirmasi Pembayaran</button>
-</div>
-=======
 @section('title', 'qris ')
 
 @section('content')
@@ -105,5 +72,4 @@
     <!-- Tombol Status -->
     <button class="btn-status">Menunggu Konfirmasi Pembayaran</button>
   </div>
->>>>>>> af738d8e807f3ba9ba1b9670d259b57387e7b5df
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,3 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('pages.home');
-});
-
-Route::get('/home', function () {
-    return view('pages.home');
-});
