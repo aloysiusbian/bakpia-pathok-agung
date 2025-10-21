ed71670273d50b7346c41f8ef923aff934abcaa0
Ruben1401 <235314179@student.usd.ac.id>
2025-10-22 02:46:03 +0700

    halo

--- database/migrations/2025_10_15_080006_create_keranjang_table.php
+++ database/migrations/2025_10_15_080006_create_keranjang_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -12,9 +13,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('keranjang', function (Blueprint $table) {
-            $table->uuid('nomorPemesanan')->constrained('pemesananonline', 'nomorPemesanan')->cascadeOnDelete()->cascadeOnUpdate();;
+            $table->uuid('idKeranjang')->primary()->default(DB::raw('UUID()'));
+            $table->foreignId('idPelanggan')->constrained('pelanggan', 'idPelanggan')->cascadeOnDelete()->cascadeOnUpdate();;
             $table->foreignId('idProduk', 7)->constrained('produk', 'idProduk')->cascadeOnDelete()->cascadeOnUpdate();;
-            $table->double('totalNota', 10, 2);
+            $table->double('subTotal', 10, 2);
+            $table->integer('jumlahBarang', 5);
         });
     }
 
