8a33fe4132286b3943e7d20db0afe68913c444e3
CedXD <cedricchandra99@gmail.com>
2025-10-21 23:17:25 +0700

    Header

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -13,12 +13,22 @@
             </div>
 
             <div class="d-flex align-items-center gap-3 ms-lg-auto">
-                <a href="/keranjang" class="text-dark fs-4">
+                <a href="/keranjang" class="text-dark fs-4" title="Dashboard Anda (Halaman Belum Dibuat)">
                     <i class="bi bi-cart"></i>
                 </a>
-                <a href="#" class="text-dark fs-4">
-                    <i class="bi bi-person"></i>
-                </a>
+                {{-- Cek apakah user belum login (Guest) --}}
+@guest
+    <a href="/login" class="btn btn-outline-dark fw-bold">
+        Login
+    </a>
+@endguest
+
+{{-- Cek apakah user sudah login (Authenticated) --}}
+@auth
+    <a href="{{ route('dashboard') }}" class="text-dark fs-4">
+        <i class="bi bi-person"></i>
+    </a>
+@endauth
             </div>
         </div>
     </nav>
