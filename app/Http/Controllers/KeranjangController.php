5956246930af132f7b913f1394930689abf96536
Aloysius Abian Dwi Hananta <aloysiusbian@gmail.com>
2025-10-22 02:51:08 +0700

    fix database

--- app/Http/Controllers/KeranjangController.php
+++ app/Http/Controllers/KeranjangController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Produk;
+use App\Models\Keranjang;
 use App\Models\DetailTransaksiOnline; // <-- GANTI: Gunakan model Keranjang dari database
 use Illuminate\Support\Facades\Auth; // <-- BARU: Diperlukan untuk mendapatkan info user yang login
 
