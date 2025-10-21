e6a77f7cd4032df66b11fb37e38b3e0c7569c803
CedXD <cedricchandra99@gmail.com>
2025-10-22 00:02:57 +0700

    Header3

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -7,7 +7,7 @@
 
             <div class="input-group flex-grow-1 mx-lg-5">
                 <input class="form-control rounded-pill border-0" placeholder="Bakpia Pathok?" aria-label="Search">
-                <span class="input-group-text bg-transparent border-0 position-absolute end-0" style="z-index: 1;" >
+                <span class="input-group-text bg-transparent border-0 position-absolute end-0" style="z-index: 1;">
                     <i class="bi bi-search text-secondary"></i>
                 </span>
             </div>
@@ -17,45 +17,53 @@
                     <i class="bi bi-cart"></i>
                 </a>
                 {{-- Cek apakah user belum login (Guest) --}}
-@guest
-    <a href="/login" class="btn btn-outline-dark fw-bold">
-        Login
-    </a>
-@endguest
+                @guest
+                    <a href="/login" class="btn btn-outline-dark fw-bold">
+                        Login
+                    </a>
+                @endguest
 
-{{-- Cek apakah user sudah login (Authenticated) --}}
-@auth
-    {{-- Dropdown Container --}}
-    <div class="dropdown">
-        {{-- Tombol Dropdown (Ikon Profil) --}}
-        <a href="#" class="text-dark fs-4 dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false"></a>
+                {{-- Cek apakah user sudah login (Authenticated) --}}
+                @auth
+                    {{-- Dropdown Container --}}
+                    <div class="dropdown">
+                        {{-- Tombol Dropdown (Ikon Profil) --}}
+                        <a href="#" class="text-dark fs-4 dropdown-toggle" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false"></a>
 
-        {{-- Menu Dropdown --}}
-        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuLink">
-            {{-- 1. Link ke Profil Akun --}}
-            <li>
-                {{-- Gunakan href="#" karena halaman profil belum dibuat, seperti permintaan sebelumnya --}}
-                <a class="dropdown-item" href="#" title="Halaman Profil Belum Dibuat">
-                    <i class="bi bi-person"></i> Profil Akun
-                </a>
-            </li>
+                        {{-- Menu Dropdown --}}
+                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuLink">
+                            {{-- 1. Link ke Profil Akun --}}
+                            <li>
+                                {{-- Gunakan href="#" karena halaman profil belum dibuat, seperti permintaan sebelumnya --}}
+                                <a class="dropdown-item" href="#"
+                                    onclick="event.preventDefault(); alert('🚧 Halaman profil akun Anda belum selesai dibuat. Silakan coba lagi nanti. 🙏');">
+                                    <i class="bi bi-person"></i> Profil Akun
+                                </a>
+                            </li>
 
-            <li><hr class="dropdown-divider"></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
 
-            {{-- 2. Link untuk Logout --}}
-            <li>
-                {{-- Form untuk Logout (Wajib untuk Laravel) --}}
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                    @csrf
-                </form>
-                <a class="dropdown-item" href="{{ route('logout') }}"
-                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                    <i class="bi bi-box-arrow-right me-2"></i> Logout
-                </a>
-            </li>
-        </ul>
-    </div>
-@endauth
+                            {{-- 2. Link untuk Logout dengan Konfirmasi --}}
+                            <li>
+                                {{-- Form POST yang dibutuhkan Laravel untuk keamanan --}}
+                                <form id="logout-form" action="/logout" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+
+                                {{-- Tautan yang memanggil konfirmasi --}}
+                                <a class="dropdown-item" href="/logout" onclick="event.preventDefault(); 
+                                if (confirm('Apakah Anda yakin ingin mengakhiri sesi Anda?')) {
+                                    document.getElementById('logout-form').submit();
+                                }">
+                                    <i class="bi bi-box-arrow-right me-2"></i> Logout
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                @endauth
             </div>
         </div>
     </nav>
